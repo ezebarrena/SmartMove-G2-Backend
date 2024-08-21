4ebe03d2fd2fc48e97849554f74430d68ab4c286
ezebarrena <ezebarrena@gmail.com>
2024-08-21 14:39:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {dbConnection} = require('./src/db/config');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//dbConnection();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello asd!')
+});
+
+app.use("/usuarios", require('./src/routes/usuarios.routes'));
+app.use("/mensajes", require('./src/routes/mensajes.routes'));
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
+
+module.exports = app;
